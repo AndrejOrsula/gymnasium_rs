84304cf49274b11e8ff30f2ab7f183b3f4dbd575
Andrej Orsula <orsula.andrej@gmail.com>
2024-01-20 19:35:53 +0100

    Reexport gymnasium_sys in gymnasium lib
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- gymnasium_sys/src/lib.rs
+++ gymnasium_sys/src/lib.rs
@@ -1,11 +1,9 @@
 //! Rust FFI bindings for Python implementation of Gymnasium.
 
-#[allow(
+#![allow(
     clippy::all,
     non_camel_case_types,
     non_snake_case,
     non_upper_case_globals
 )]
-pub mod gym {
-    include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-}
+include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
